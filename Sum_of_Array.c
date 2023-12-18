6565920acbb1d43f75d32eca76c4e0197d5dd922
swethamadda <151734714+swethamadda@users.noreply.github.com>
2023-12-18 15:02:11 +0530

    //sum of all the elements in the given array #include<stdio.h> int main() { 	int n,sum=0; 	scanf("%d",&n); 	int arr[n],i; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	for(i=0;i<n;i++) 	{ 		sum=sum+arr[i]; 	} 	printf("%d",sum); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,17 @@
+//sum of all the elements in the given array
+#include<stdio.h>
+int main()
+{
+	int n,sum=0;
+	scanf("%d",&n);
+	int arr[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+arr[i];
+	}
+	printf("%d",sum);
+}

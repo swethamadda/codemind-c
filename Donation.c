3d4e40a905cd09726c3f8b0d022e9893078f0631
swethamadda <151734714+swethamadda@users.noreply.github.com>
2024-01-02 21:57:12 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     int diff=y-x;     if(y>x)     {         printf("%d",diff);     }     else     {         printf("x ");     } }

---
+++ Donation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int diff=y-x;
+    if(y>x)
+    {
+        printf("%d",diff);
+    }
+    else
+    {
+        printf("x
+");
+    }
+}
